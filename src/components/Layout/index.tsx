bf5610824577ea9db4af849ab3902f37d6cdd8a8
keep-calm-and-carry-off <rztitan@ya.ru>
2024-01-16 01:27:50 +0300

    add Product List

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,15 +3,13 @@ import Header from "../Header";
 import cn from 'classnames';
 import * as styles from './styles.module.scss'
 
-export interface ILayout extends HTMLProps<HTMLDivElement> { }
+export interface ILayout extends HTMLProps<HTMLDivElement> {}
 
-const Layout: FC<ILayout> = (props: ILayout) => {
-    return <>
-        <div className={cn(styles.layout, props.className)} {...props}>
-            <Header />
-            {props.children}
-        </div>
-    </>
+const Layout:FC<ILayout> = (props:ILayout) => {
+    return <div className={cn(styles.layout, props.className)} {...props}>
+        <Header/>
+        {props.children}
+    </div>
 }
 
 export default Layout
