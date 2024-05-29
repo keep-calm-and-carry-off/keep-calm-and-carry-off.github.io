78ca0cb9e2a9da5282064f9abd473977a650fa56
keep-calm-and-carry-off <rztitan@ya.ru>
2024-05-29 10:57:27 +0300

    изменил на includes проверку логина

--- src/hooks/useAccessLevel.ts
+++ src/hooks/useAccessLevel.ts
@@ -5,7 +5,7 @@ import { getProfile } from 'src/stores/sagaStore/slices/user';
 export const useAccessLevel = () => {
   const profile = useSelector(getProfile);
 
-  if (profile && (profile.email === 'skiper94@ya.ru' || profile.email.startsWith('admin'))) return 777;
+  if (profile && (profile.email === 'skiper94@ya.ru' || profile.email.includes('admin'))) return 777;
   else if (profile) return 1;
   else return -1;
 };
